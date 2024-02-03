416fe4bccd2528fe1689bc7e6a20af5b6b073ab5
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-02-03 21:16:19 +0300

    feat: :sparkles: attendance edit

--- employee_attendance/lib/screens/attendance_screen.dart
+++ employee_attendance/lib/screens/attendance_screen.dart
@@ -12,9 +12,52 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child:  Text("Attendance Today"),
-      ),
+      body : SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child:  Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top : 32),
+              child: const Text("Welcome", style: TextStyle(
+                color: Colors.black54,
+                fontSize: 30
+              ),
+              ),
+
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const  Text("Employee Name",style: TextStyle(
+                fontSize: 25
+              ),),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 32),
+              child: const  Text(
+                "Today's Status"
+              ,style: TextStyle(
+                fontSize: 20
+              ),),
+            ),
+            Container(
+              margin: const EdgeInsets.only(32),
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(2, 2)
+                    )
+                ]
+              ),
+            ).
+          ]),
+      )
+
     );
   }
 }
