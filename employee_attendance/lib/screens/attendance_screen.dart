654c7aea647a787f1dbc8c78f57a48fefad8ee44
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-02-04 14:35:58 +0300

    feat: :sparkles: Home page design

--- employee_attendance/lib/screens/attendance_screen.dart
+++ employee_attendance/lib/screens/attendance_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slide_to_act/slide_to_act.dart';
 
 
 class AttendanceScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class AttendanceScreen extends StatefulWidget {
 }
 
 class _AttendanceScreenState extends State<AttendanceScreen> {
+  final GlobalKey<SlideActionState> key = GlobalKey<SlideActionState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +44,7 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
               ),),
             ),
             Container(
-              margin: const EdgeInsets.only(32),
+              margin: const EdgeInsets.only(top: 32),
               height: 150,
               decoration: const BoxDecoration(
                 color: Colors.white,
@@ -52,12 +54,72 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
                     blurRadius: 10,
                     offset: Offset(2, 2)
                     )
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text("Check in", style:  TextStyle(fontSize: 20, color: Colors.black54),),
+                        SizedBox(
+                          width: 80,
+                          child: Divider(),),
+                          Text("9:30", style: TextStyle(fontSize: 25),)
+                      ],)),
+
+                    Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text("Check out", style:  TextStyle(fontSize: 20, color: Colors.black54),),
+                        SizedBox(
+                          width: 80,
+                          child: Divider(),),
+                          Text(
+                            "--/--", 
+                            style: TextStyle(fontSize: 25),)
+                      ],))
                 ]
               ),
-            ).
-          ]),
-      )
+            ),
+            Container(
+              margin: const EdgeInsets.only(top : 25),
+              alignment: Alignment.centerLeft,
+              child: Text("02 Feb 2024", style: TextStyle(fontSize: 20),),
+            ),
 
-    );
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text("02:02:55 PM", style: TextStyle(fontSize: 15, color: Colors.black54),),
+            ),
+
+            Container(
+              margin: const EdgeInsets.only(top : 25),
+              child: Builder(builder: (context){
+                return SlideAction(
+                  text: "Slide to check out",
+                  textStyle: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                  ),
+                  outerColor: Colors.white,
+                  innerColor: Colors.blueGrey,
+                  key : key,
+                  onSubmit: (){
+                    key.currentState!.reset();
+                  },
+                );
+              }),
+            )
+          ],
+          ),
+      ));
   }
 }
