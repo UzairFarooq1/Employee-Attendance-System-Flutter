4af330f1799db5b25647ef612dd59cc287b29c35
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-02-06 21:08:08 +0300

    feat: :sparkles: create and design profile page

--- employee_attendance/lib/screens/profile_screen.dart
+++ employee_attendance/lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
+import 'package:employee_attendance/models/department_model.dart';
+import 'package:employee_attendance/services/db_service.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -9,12 +11,84 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  TextEditingController nameController = TextEditingController();
+  int selectedValue = 0;
+
   @override
   Widget build(BuildContext context) {
+    final dbService = Provider.of<DbService>(context);
+
+    dbService.allDepartments.isEmpty ? dbService.getAllDepartments() : null;
+    nameController.text.isEmpty
+        ? nameController.text = dbService.userModel?.name ?? ''
+        : null;
     return Scaffold(
-      body: Center(
-        child:  Text("Profile"),
-      ),
-    );
+        body: dbService.userModel == null
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 80),
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blueGrey,
+                        ),
+                        child: const Center(
+                          child: Icon(Icons.person_outline_sharp,
+                              size: 60, color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text("Employee ID : ${dbService.userModel?.employeeId}"),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        controller:  nameController,
+                        decoration: const InputDecoration(
+                          label: Text("Full Name"),
+                          border: OutlineInputBorder()
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      dbService.allDepartments.isEmpty? const LinearProgressIndicator() : SizedBox(
+                        width: double.infinity,
+                        child: DropdownButtonFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+
+                            )
+                          ),
+                          value: dbService.employeeDepartment ?? dbService.allDepartments.first.id,
+                          items: dbService.allDepartments.map((DepartmentModel item) {
+                            return DropdownMenuItem(
+                              value: item.id ,
+                              child: Text(item.title, style:  const TextStyle(
+                                fontSize: 18,
+                              ),)
+                            );
+                          }).toList(),
+                          onChanged:(selectedValue){
+                            dbService.employeeDepartment = selectedValue;
+                          }
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ));
   }
 }
--- employee_attendance/lib/services/db_service.dart
+++ employee_attendance/lib/services/db_service.dart
@@ -1,13 +1,17 @@
 import 'dart:math';
 
 import 'package:employee_attendance/constants/constants.dart';
+import 'package:employee_attendance/models/department_model.dart';
 import 'package:employee_attendance/models/user_model.dart';
+import 'package:employee_attendance/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DbService extends ChangeNotifier {
   final SupabaseClient _supabase = Supabase.instance.client;
   UserModel? userModel;
+  List<DepartmentModel> allDepartments = [];
+  int? employeeDepartment;
 
   String generateRandomEmployeeId() {
     final random = Random();
@@ -35,6 +39,29 @@ class DbService extends ChangeNotifier {
         .eq('id', _supabase.auth.currentUser!.id)
         .single();
     userModel = UserModel.fromJson(userData);
+    employeeDepartment == null 
+    ? employeeDepartment = userModel?.department 
+    : null;
     return userModel!;
+  }
+
+  Future<void> getAllDepartments() async{
+    final List result =
+        await _supabase.from(Constants.departmentTable).select();
+      allDepartments = 
+        result.map((department) => 
+        DepartmentModel.fromJson(department))
+        .toList();
+      notifyListeners();  
+  }
+
+  Future updateProfile(String name, BuildContext context) async{
+    await _supabase.from(Constants.employeeTable).update({
+      'name':name,
+      'department': employeeDepartment,
+  }).eq('id', _supabase.auth.currentUser!.id);
+
+  Utils.ShowSnackbar("Profile Updated Successfully", context, color: Colors.greenAccent);
+  notifyListeners();
   }
 }
